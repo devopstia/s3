4594d1fdb2daf631487034c9e4f4b950022af4f0
devopseasylearning <info@devopseasylearning.com>
2023-04-10 20:04:13 -0500

    Changed the s3 bucket name

--- session03/resources/ec2/dev/main.tf
+++ session03/resources/ec2/dev/main.tf
@@ -17,10 +17,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-s3"
+    bucket         = "2560-dev-alpha-s3-backend"
     key            = "ec2/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "my-terraform-state-locks-s3"
+    dynamodb_table = "2560-dev-alpha-s3-dynamodb-table"
   }
 }
 
